604a9949bd986e675d144a23d9458df976d66f77
hennenfuss49dev <104261441+hennenfuss49dev@users.noreply.github.com>
2025-03-03 14:00:35 +0100

    Hennenfuss49.java added

---
+++ Hennenfuss49.java
@@ -0,0 +1,5 @@
+public class Hennenfuss49 {
+    void print() {
+        System.out.println("Hennenfuss49");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Test!");
-        System.out.println("SWP is supa3");
+        new Hennenfuss49().print();
     }
 
 }
